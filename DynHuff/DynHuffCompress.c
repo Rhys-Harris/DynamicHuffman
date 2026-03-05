9641f1ba34217ea3e952a02a96f8763f680c248c
Rhys-Harris <harrt002@mymail.unisa.edu.au>
2026-03-05 13:09:34 +1030

    Impl helper functions that read from file

--- DynHuff/DynHuffCompress.c
+++ DynHuff/DynHuffCompress.c
@@ -1,9 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
 #include "DynHuffCompress.h"
 
-errno_t dynHuffCompress(const char *text, const char *outfilename) {
-	return 1;
-}
+#define MAX_CHARS 100000
 
 errno_t dynHuffCompressFile(const char *infilename, const char *outfilename) {
+	// Read in text
+	FILE *f;
+	if (fopen_s(&f, infilename, "rb")) {
+		printf("Couldn't open input file\n");
+		return 1;
+	}
+
+	char text[MAX_CHARS];
+	memset(text, 0, MAX_CHARS);
+
+	fread(text, sizeof(char), MAX_CHARS, f);
+
+	fclose(f);
+
+	return dynHuffCompress(text, outfilename);
+}
+
+errno_t dynHuffCompress(const char *text, const char *outfilename) {
 	return 1;
 }
--- DynHuff/DynHuffDecompress.c
+++ DynHuff/DynHuffDecompress.c
@@ -1,9 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
 #include "DynHuffDecompress.h"
 
-errno_t dynHuffDecompress(const char *compText, const char *outfilename) {
-	return 1;
-}
+#define MAX_CHARS 100000
 
 errno_t dynHuffDecompressFile(const char *infilename, const char *outfilename) {
+	// Read in text
+	FILE *f;
+	if (fopen_s(&f, infilename, "rb")) {
+		printf("Couldn't open input file\n");
+		return 1;
+	}
+
+	char text[MAX_CHARS];
+	memset(text, 0, MAX_CHARS);
+
+	fread(text, sizeof(char), MAX_CHARS, f);
+
+	fclose(f);
+
+	return dynHuffDecompress(text, outfilename);
+}
+
+errno_t dynHuffDecompress(const char *compText, const char *outfilename) {
 	return 1;
 }
